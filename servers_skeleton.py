c549bca89bccf9f81911b318a0cdb8b43edc0615
Karol <lolo10inpg@gmail.com>
2019-12-02 21:34:36 +0100

    class Server and Product

--- servers_skeleton.py
+++ servers_skeleton.py
@@ -1,20 +1,42 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from typing import Optional
+# Karol Talaga 302929
+# Zbigniew Żeglecki 302947
+from typing import Optional, List, Dict
+from abc import ABC, abstractmethod
 
 
 class Product:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
+    def __init__(self, product_name: str, price: float):
+        self.product_name = product_name
+        self.price = price
+
+
+class TooManyProductsFoundError(Exception):
     pass
 
 
-class TooManyProductsFoundError:
-    # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
-    pass
+# FIXME: Każda z poniższych klas serwerów powinna posiadać: (1) metodę inicjalizacyjną przyjmującą listę obiektów typu
+#  `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze, (2) możliwość
+#  odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę
+#  wyników wyszukiwania, (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów
+#  spełniających kryterium wyszukiwania
 
+class Server(ABC):
+    def __init__(self, list_of_products: List[Product], *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.list_of_products = list_of_products
 
-# FIXME: Każada z poniższych klas serwerów powinna posiadać: (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze, (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania, (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
+    n_max_returned_entries = 3
+
+    def make_dict(self) -> Dict[str, Product]:
+        dict_of_products = {}
+        for product in self.list_of_products:
+            dict_of_products[product.product_name] = product
+        return dict_of_products
+
+    @abstractmethod
+    def get_entries(self, n: int = 1):
+        raise NotImplementedError
+
 
 class ListServer:
     pass
@@ -25,7 +47,8 @@ class MapServer:
 
 
 class Client:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
+    pass
+
 
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
         raise NotImplementedError()
