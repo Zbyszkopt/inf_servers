68b778895abb1f9ff56a43d6fe863ffba0009f7e
Karol <lolo10inpg@gmail.com>
2019-12-03 21:52:29 +0100

    class ListServer done, clas Product updated

--- servers_skeleton.py
+++ servers_skeleton.py
@@ -11,43 +11,54 @@ class Product:
         self.name = name
         self.price = price
 
+    def __hash__(self):
+        return hash((self.name, self.price))
+
+    def __eq__(self, other):
+        return self.name == other.name and self.price == other.price
+
 
 class TooManyProductsFoundError(Exception):
     pass
 
 
-# FIXME: Każda z poniższych klas serwerów powinna posiadać: (1) metodę inicjalizacyjną przyjmującą listę obiektów typu
-#  `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze, (2) możliwość
-#  odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę
-#  wyników wyszukiwania, (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów
-#  spełniających kryterium wyszukiwania
-
 class Server(ABC):
-    def __init__(self, list_of_products: List[Product], *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, list_of_products: List[Product]) -> None:
         self.list_of_products = list_of_products
 
     n_max_returned_entries = 3
 
     def make_dict(self) -> Dict[str, Product]:
-        dict_of_products = {}
-        for product in self.list_of_products:
-            dict_of_products[product.product_name] = product
+        dict_of_products = {product.name: product for product in self.list_of_products}
         return dict_of_products
 
     @abstractmethod
-    def get_entries(self, n: int = 1):
+    def get_entries(self, n_letters: int = 1):
         raise NotImplementedError
 
 
 class ListServer:
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_entries(self, n_letters: int = 1) -> List[Product]:
+        wishlist = []
+        for prod in self.list_of_products:
+            let_ = re.split('(\d+)', prod.name)[0]
+            num_ = re.split('(\d+)', prod.name)[1]
+            if len(num_) == 2 or len(num_) == 3:
+                if len(let_):
+                    wishlist.append(product)
+        if len(wishlist) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError()
+        sorted_wishlist = sorted(wishlist, key=lambda product: product.price)
+        return sorted_wishlist
 
 
 class MapServer(Server):
 
-    def __init__(self, list_of_products: List[Product], n_max_returned_entries: int, *args, **kwargs) -> None:
-        super().__init__(list_of_products, n_max_returned_entries, *args, **kwargs)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.dict_of_products = self.make_dict()
 
     def get_entries(self, n_letters: int = 1) -> List[Product]:
